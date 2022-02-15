de6adcc3524c8903175f6c402ec2888e5decce56
0b1kn00b <polysemantic@gmail.com>
2022-02-15 13:05:31 +0100

    EXCEPT becomes REJECT

--- src/main/haxe/stx/test/Util.hx
+++ src/main/haxe/stx/test/Util.hx
@@ -9,10 +9,10 @@ class Util{
       __.reject(e.except().errate(E_Test_Rejection));
     }catch(e:haxe.Exception){
       trace(e.stack);
-      __.reject(Rejection.make(Some(EXCEPT(E_Test_Exception(e))),None,pos));
+      __.reject(Rejection.make(Some(REJECT(E_Test_Exception(e))),None,pos));
     }catch(e:Dynamic){
       trace(e);
-      __.reject(Rejection.make(Some(EXCEPT(TestFailedBecause('$e'))),None,pos));
+      __.reject(Rejection.make(Some(REJECT(TestFailedBecause('$e'))),None,pos));
     }
     //return __.accept(fn());
   }
